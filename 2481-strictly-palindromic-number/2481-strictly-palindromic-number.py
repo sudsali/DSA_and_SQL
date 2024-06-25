462ef2523d4db4ddc9c2b7ea231aefc3ab202318
Sudhanshu Sali <salisudhanshu@gmail.com>
2024-06-25 01:13:29 -0400

    Time: 34 ms (62.13%), Space: 16.4 MB (93.84%) - LeetHub

---
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.py
@@ -0,0 +1,4 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        # For any n >= 4, the function should return False because the answer for any n-2 representation is 12 which is not palindorme
+        return False
