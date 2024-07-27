e2e46f7fbfc0b080f28d974b0b1bbc8d27bc1974
Sudhanshu Sali <salisudhanshu@gmail.com>
2024-07-26 20:13:07 -0400

    Time: 67 ms (8.27%) | Memory: 16.7 MB (13.16%) - LeetSync

---
+++ 443-string-compression/string-compression.py
@@ -0,0 +1,23 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        s = ""
+        curr = chars[0]
+        count = 0
+        if len(chars) == 1:
+            return len(chars)
+        for i in range(len(chars)):
+            if curr != chars[i]:
+                s+=curr
+                if count > 1:
+                    s+=str(count)
+                curr = chars[i]
+                count = 1
+            else:
+                count+=1
+        s+= curr
+        if count > 1:
+            s+= str(count)
+        chars.clear()
+        for i in s:
+            chars+=i            
+        return len(chars)
