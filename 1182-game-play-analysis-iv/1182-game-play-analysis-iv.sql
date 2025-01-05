590c6bdfffb6920fc5cb6737554dbe6d13d8f4b4
Sudhanshu Sali <salisudhanshu@gmail.com>
2025-01-04 20:19:07 -0500

    Time: 1326 ms (13.3%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
-WITH Firstlogin AS (
-    SELECT player_id, MIN(event_date) as first_login
+WITH FirstLogin AS (
+    SELECT player_id , MIN(event_date) as first_login
     FROM Activity
     GROUP BY player_id
 ),
-Secondlogin AS(
+SecondLogin AS (
     SELECT a.player_id
     FROM Activity a
-    JOIN Firstlogin b ON a.player_id = b.player_id
+    JOIN FirstLogin b ON a.player_id = b.player_id
     WHERE DATEDIFF(a.event_date, b.first_login) = 1
 )
-SELECT ROUND(COUNT(DISTINCT Secondlogin.player_id) /  (SELECT COUNT(DISTINCT player_id) FROM Activity),2) AS Fraction
-FROM Secondlogin;
+SELECT ROUND(COUNT(DISTINCT SecondLogin.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity),2) AS fraction
+FROM SecondLogin;
