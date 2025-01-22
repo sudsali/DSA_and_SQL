7d028c616520a0aa3ec8909ca82a56597ab8cb22
Sudhanshu Sali <salisudhanshu@gmail.com>
2025-01-22 11:07:10 -0800

    Time: 591 ms (27.33%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+WITH accepted AS (
+    SELECT requester_id AS id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+)
+SELECT id, COUNT(*) AS num
+FROM accepted
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
