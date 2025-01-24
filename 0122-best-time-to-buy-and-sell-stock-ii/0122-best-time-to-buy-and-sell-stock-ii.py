1cb074e76511d811031c02f5b781af84441e7ef9
Sudhanshu Sali <salisudhanshu@gmail.com>
2025-01-23 20:02:30 -0800

    Time: 3 ms (38.58%), Space: 18.7 MB (48.26%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,8 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        curr_low = prices[0]
-        curr_high = prices[0]
         curr_sum = 0
         for i in range(len(prices)-1):
             curr_sum = prices[i+1] - prices[i]
