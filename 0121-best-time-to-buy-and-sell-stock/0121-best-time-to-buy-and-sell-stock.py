61c09472dc906b267fb3514250c47d3078361c2d
Sudhanshu Sali <salisudhanshu@gmail.com>
2025-02-11 10:40:19 -0500

    Time: 128 ms (28.99%), Space: 27 MB (31.51%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,4 +1,5 @@
 class Solution:
+    # here we need to keep track of the minimum cost and if current price is greater than min_cost we calculate max profit
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
         min_buy = prices[0]
