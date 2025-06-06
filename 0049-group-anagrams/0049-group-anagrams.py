097953d5ec3df9e79b4cd53f68b340bfeed11dba
Sudhanshu Sali <salisudhanshu@gmail.com>
2025-06-06 02:02:25 -0700

    Time: 2273 ms (5.06%), Space: 22.8 MB (5.56%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,25 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        heap3 = collections.defaultdict(list)
+        pattern = set()
+        heap2 = {}
+        for index, val in enumerate(strs):
+            heap2[index] = "".join(sorted(val))
+            pattern.add(heap2[index])
+
+        print(pattern)
+        print(heap2)
+    
+        i = -1
+        for p in pattern:
+            i+=1
+            for key,val in heap2.items():
+                if p == val:
+                    heap3[i].append(strs[key])
+
+        print(heap3)
+
+        res = []
+        for key, val in heap3.items():
+            res.append(val)
+        return res
