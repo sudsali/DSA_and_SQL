1565ccad41ac0fae17d599263eaa609a7b83c3f7
Sudhanshu Sali <salisudhanshu@gmail.com>
2025-06-06 23:31:18 -0700

    Time: 31 ms (20.85%) | Memory: 19.4 MB (39.35%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,21 +1,20 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        l_wall = r_wall = 0
+        n = len(height)
+        max_left = [0] * n
+        max_right = [0] * n
+        res = 0
 
-        # find 2 arrs: max_left for each index and max_right for each index
-        # Then water will be sum of min(max_left[i],max_right[i]) - height[i]
-        # remember to not consider the last index when calculating water 
-        water = 0
-        max_left = [0] * len(height)
-        max_right = [0] * len(height)
+        for i in range(n):
+            j = -i - 1
+            max_left[i] = l_wall
+            max_right[j] = r_wall
+            l_wall = max(l_wall,height[i])
+            r_wall = max(r_wall,height[j])
 
-        max_right[-1] = height[-1]
-        for i in range(len(height)-2,-1,-1):
-            max_right[i] = max(max_right[i+1],height[i])
-            
-        max_left[0] = height[0]
-        for i in range(1,len(height)):
-            max_left[i] = max(max_left[i-1],height[i])
+        for i in range(n):
+            pot = min(max_left[i],max_right[i])
+            res += max(0,pot - height[i])
 
-        for i in range(len(height)-1):
-            water += min(max_left[i],max_right[i]) - height[i]
-        return water
+        return res
