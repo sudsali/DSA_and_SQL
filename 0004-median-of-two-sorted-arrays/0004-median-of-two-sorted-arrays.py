893f89ed2b95297520106e641e1a165d12367cc8
Sudhanshu Sali <salisudhanshu@gmail.com>
2025-06-11 00:21:03 -0700

    Time: 0 ms (100%), Space: 18.2 MB (39.66%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,24 +1,28 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        p1 = 0
-        p2 = 0
-        i=0
-        total_len = len(nums1) + len(nums2)
-        is_even = (total_len % 2 == 0)
-        mid = total_len // 2
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
 
-        res = []
-        while len(res) <= mid:
-            if p1 < len(nums1) and (p2 >= len(nums2) or nums1[p1] <= nums2[p2]):
-                res.append(nums1[p1])
-                p1+=1
+        m, n = len(nums1), len(nums2)
+        total = m + n
+        half = total // 2
+
+        left, right = 0, m
+        while left <= right:
+            i = (left + right) // 2
+            j = half - i
+
+            Aleft = nums1[i - 1] if i > 0 else float("-inf")
+            Aright = nums1[i] if i < m else float("inf")
+            Bleft = nums2[j - 1] if j > 0 else float("-inf")
+            Bright = nums2[j] if j < n else float("inf")
+            
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2 == 0:
+                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+                else:
+                    return min(Aright, Bright)
+            elif Aleft > Bright:
+                right = i - 1
             else:
-                res.append(nums2[p2])
-                p2+=1
-
-        if is_even:
-            median = (res[-1]+res[-2])/2
-        else:
-            median = res[-1]
-
-        return median
+                left = i + 1
