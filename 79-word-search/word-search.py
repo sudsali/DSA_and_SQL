d60dd3e79bc7bf1ae1728db8919b8d32bbd4e8e1
Sudhanshu Sali <salisudhanshu@gmail.com>
2025-06-12 12:57:50 -0700

    Time: 4674 ms (36.74%) | Memory: 18.2 MB (21.26%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,32 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        hashmap = defaultdict(list)
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                hashmap[board[i][j]].append([i,j])
+
+        if word[0] in hashmap:
+            index = hashmap[word[0]]
+        else:
+            return False
+
+        rows, cols = len(board), len(board[0])
+        
+        def dfs(i,j,k,visited):
+            if k == len(word):
+                return True
+            
+            if (i<0 or i>=rows or j<0 or j>=cols or (i,j) in visited or board[i][j]!=word[k]):
+                return False
+            
+            visited.add((i,j))
+            res = (dfs(i+1,j,k+1,visited) or dfs(i-1,j,k+1,visited) or dfs(i,j+1,k+1,visited) or dfs(i,j-1,k+1,visited))
+            visited.remove((i,j))
+            return res
+        
+        while index:
+            i,j = index.pop()
+            if dfs(i,j,0,set()):
+                return True 
+
+        return False
